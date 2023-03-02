1604eca51fd04b10698a3932a19738d63362cb78
Nash Pillai <nash.pillai@protonmail.com>
2023-03-02 10:11:48 -0500

    make it a pwa

--- FtcDashboard/dash/vite.config.ts
+++ FtcDashboard/dash/vite.config.ts
@@ -3,9 +3,31 @@ import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import path from 'path';
 
+import { VitePWA } from 'vite-plugin-pwa';
+
 export default defineConfig({
   base: '/dash/',
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    svgr(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'Jaybots Dashboard',
+        short_name: 'Jay-Dash',
+        description: 'Configure variables and test roadrunner paths',
+        theme_color: '#ffffff',
+        icons: [
+          { src: 'pwa-192x192.png', sizes: '192x192', type: 'image/png' },
+          { src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png' },
+        ],
+      },
+    }),
+  ],
   server: {
     port: 8000,
     hmr: { host: 'localhost' },
